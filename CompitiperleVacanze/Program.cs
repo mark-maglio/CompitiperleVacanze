83da33f78c6378eb475aad3372dcee040e8cf220
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-20 13:26:06 +0100

    continuo2....

--- CompitiperleVacanze/Program.cs
+++ CompitiperleVacanze/Program.cs
@@ -8,11 +8,14 @@ namespace CompitiperleVacanze
         {
             int a = int.MaxValue;
             Console.WriteLine(a);//qua il codice stampa il numero massimo
-            Console.WriteLine(a+1);//qua si nota un'errore perchè a va in overflow per qui stampa 1
-            int b = -128;
-            int c = -b;
-            Console.WriteLine(Convert.ToString(b,2));
-            Console.WriteLine(Convert.ToString(c,2));
+            while(a != int.MaxValue)
+            {
+                Console.WriteLine(a+1);//qua si nota un'errore perchè a va in overflow per cui stampa 1
+            }
+            int b = -128;//viene assegnato un valore alla variabile
+            int c = -b;//viene assegnato -b a c
+            Console.WriteLine(Convert.ToString(b,2));//il valore della variabile b viene convertito in binario
+            Console.WriteLine(Convert.ToString(c,2));//il valore della variabile c viene convertito in binario
             int value = 31;
             int somma = value;
             value = ~value + 1; //NOT
